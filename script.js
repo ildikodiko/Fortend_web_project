0dcffb1e3ed43e85c739e182b866c73b321d19cd
ildikodiko <59513666+ildikodiko@users.noreply.github.com>
2020-02-11 20:37:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+// DOM Elements
+
+const mainPage = document.querySelector('.main-page');
+const loginPage = document.querySelector('.login-page');
+const middleContent = document.querySelector('.middle-content');
+const btnTop = document.querySelector('.btn-top');
+const newsFeedPage = document.querySelector('.feeds-page');
+const loginModal = document.querySelector('.login-modal');
+const HomeBtn = document.querySelector('.brand');
+const modalX = document.querySelector('.login-modal i');
+const loginFormBtn = document.querySelector('.login-form-btn');
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postModalX = document.querySelector('.modal-header i');
+const modalPostBtn = document.querySelector('.modal-header button');
+const modalFooterPlus = document.querySelector('.modal-footer span');
+const modalInput = document.querySelector('.modal-input');
+const user = document.querySelector('.user');
+const sidebar = document.querySelector('.sidebar');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const sidebarX = document.querySelector('.sidebar-header i');
+const toggle = document.querySelector('.toggle');
+const circle = document.querySelector('.circle');
+
+/*****************************************************************/
+/*****************************************************************/
+
+//Main page
+
+const goToLoginPage = () => {
+	mainPage.style.display = 'none';
+	loginPage.style.display = 'grid';
+};
+
+//eventlistener to buttons parent elements
+//e.target gives the element that is clicked within the parent element (buttons)
+//e.target.classList gives the classes of the elment in an array
+middleContent.addEventListener('click', (e) => {
+	if (e.target.classList[1] === 'main-btn') {
+		goToLoginPage();
+	}
+});
+
+btnTop.addEventListener('click', () => {
+	const inputUserInfo = document.querySelector('.user-info');
+	const inputPassword = document.querySelector('.password');
+
+	if (inputUserInfo.value !== '' && inputPassword.value !== '') {
+		mainPage.style.display = 'none';
+		newsFeedPage.style.display = 'block';
+	} else {
+		goToLoginPage();
+		loginModal.style.display = 'block';
+	}
+});
+
+//Login page
+
+HomeBtn.addEventListener('click', () => {
+	mainPage.style.display = 'grid';
+	loginPage.style.display = 'none';
+})
+
+modalX.addEventListener('click', () => {
+	loginModal.style.display = 'none';
+});
+
+loginFormBtn.addEventListener('click', () => {
+	const loginUserInfo = document.querySelector('.login-user-info');
+	const loginPassword = document.querySelector('.login-password');
+	if (loginUserInfo.value !== '' && loginPassword.value !== '') {
+		loginPage.style.display = 'none';
+		newsFeedPage.style.display = 'block';
+	} else {
+		loginModal.style.display = 'block';
+	}
+});
+
+// News feed page
+// Post modal
+
+const changeOpacity = (x) => {
+	modalPostBtn.style.opacity = x;
+	modalFooterPlus.style.opacity = x;
+}
+
+postBtn.addEventListener('click', () => {
+	modal.style.display = 'block';
+	modalWrapper.classList.add('modal-wrapper-display');
+});
+
+postModalX.addEventListener('click', () => {
+	modal.style.display = 'none';
+	modalWrapper.classList.remove('modal-wrapper-display');
+	if (modalInput.value !== '') {
+		modalInput.value = '';
+		changeOpacity(0.5);
+	}
+});
+
+modalInput.addEventListener('keypress', (e) => {
+		changeOpacity(1);
+});
+
+modalInput.addEventListener('blur', (e) => {
+	if (e.target.value === '') {
+		changeOpacity(0.5);
+	}
+});
+
+//Side Bar
+
+user.addEventListener('click', () => {
+	sidebar.classList.add('sidebar-display');
+	sidebarWrapper.classList.add('sidebar-wrapper-display');
+});
+
+sidebarX.addEventListener('click', () => {
+	sidebar.classList.remove('sidebar-display');
+	sidebarWrapper.classList.remove('sidebar-wrapper-display');
+});
+
+// Dark Mode
+
+const darkElements1 = document.querySelectorAll('.dark-mode-1'); //egy listát készít, de nekem array kell
+const darkElements2 = document.querySelectorAll('.dark-mode-2');
+const lightText = document.querySelectorAll('.light-text');
+const borders = document.querySelectorAll('.border');
+
+toggle.addEventListener('click', () => {
+	circle.classList.toggle('move');
+	Array.from(darkElements1).map(darkelement1 => 
+		darkelement1.classList.toggle('dark-1')
+	); //a listából, arrayt készítettünk az array.from-al majd mappaltük(mint foreach)
+	Array.from(darkElements2).map(darkelement2 => 
+		darkelement2.classList.toggle('dark-2')
+	);
+	Array.from(lightText).map(lightelements => 
+		lightelements.classList.toggle('light')
+	);
+	Array.from(borders).map(borderelement => 
+		borderelement.classList.toggle('border-color')
+	);
+});
+
+
